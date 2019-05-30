7609f2fdffdec82e4d057c988251d579bda20f5b
Kizmet <bryantpatton@gmail.com>
2019-05-30 07:34:02 -0700

    updates to forms

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 
+
 class TodoForm extends React.Component {
     render() {
+        const inputText = this.props.inputText;
+        
         return (
             <form>
-                <input type="text" placeholder="Add ToDo"></input>
-                <input type="button" value="Add Tasks"/>
+                <input 
+                value={inputText}
+                type="text" 
+                placeholder="Add ToDo"
+                />
+                <input 
+                type="button" 
+                //value="Add Tasks"
+                onClick = {this.value.onClick}
+                />
                 <input type="button" value="Remove Completed"/>
             </form>
         )
